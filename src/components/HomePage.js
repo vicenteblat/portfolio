a56d0f5121df47205c8c96cacfa2dfcd4e085237
Vicente Blat <blat98v@gmail.com>
2020-07-10 18:31:42 -0400

    Fix backgroung image filter + welcome message

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 import '../css/HomePage.css';
 
 class HomePage extends Component {
   render() {
     return (
-      <div className="background">
-        <h1 className="welcome-message">Hi, I’m Vicente!</h1>
-      </div>
+      <>
+        <div className="background-image"></div>
+        <div className="content">
+          <h1 className="welcome-message">Hi, I’m Vicente!</h1>
+          <p className="intro">
+            I'm a Senior Biomedical Engineeering Student at Georgia Tech
+          </p>
+        </div>
+      </>
     );
   }
 }
