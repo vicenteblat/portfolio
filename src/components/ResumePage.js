7b94c7b986a3567ca8215677cfb2dbc7082f9519
Vicente Blat <blat98v@gmail.com>
2020-07-14 02:09:41 -0400

    Add new background images

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -5,27 +5,31 @@ import '../css/ResumePage.css';
 class ResumePage extends Component {
   render() {
     return (
-      <div id="Resume" className="resume-wrapper">
+      <div id="Resume" className="resume-wrapper resume-background">
         <section className="resume-section">
           <Container>
             <h1 className="resume-title d-flex justify-content-center">
               Resume
             </h1>
-            <h2 className="d-flex justify-content-center">Education</h2>
+            <h2 className="category-title d-flex justify-content-center">
+              Education
+            </h2>
             <p className="resume-paragraph d-flex justify-content-center">
               Lorem ipsum dolor sit amet, vim affert alterum ne. An his adipisci
               dissentias. Vidit debitis in his. Cu cum graeco prodesset, no has
               case senserit temporibus. Minim solet vis eu. An mazim liber mei,
               ius copiosae petentium contentiones ei.
             </p>
-            <h2 className="d-flex justify-content-center">Relevant Courses</h2>
+            <h2 className="category-title d-flex justify-content-center">
+              Relevant Courses
+            </h2>
             <p className="resume-paragraph d-flex justify-content-center">
               Lorem ipsum dolor sit amet, vim affert alterum ne. An his adipisci
               dissentias. Vidit debitis in his. Cu cum graeco prodesset, no has
               case senserit temporibus. Minim solet vis eu. An mazim liber mei,
               ius copiosae petentium contentiones ei.
             </p>
-            <h2 className="d-flex justify-content-center">
+            <h2 className="category-title d-flex justify-content-center">
               Professional Experience
             </h2>
             <p className="resume-paragraph d-flex justify-content-center">
@@ -34,7 +38,7 @@ class ResumePage extends Component {
               case senserit temporibus. Minim solet vis eu. An mazim liber mei,
               ius copiosae petentium contentiones ei.
             </p>
-            <h2 className="d-flex justify-content-center">
+            <h2 className="category-title d-flex justify-content-center">
               Leadership Experience
             </h2>
             <p className="resume-paragraph d-flex justify-content-center">
@@ -43,7 +47,9 @@ class ResumePage extends Component {
               case senserit temporibus. Minim solet vis eu. An mazim liber mei,
               ius copiosae petentium contentiones ei.
             </p>
-            <h2 className="d-flex justify-content-center">Skills</h2>
+            <h2 className="category-title d-flex justify-content-center">
+              Skills
+            </h2>
             <p className="resume-paragraph d-flex justify-content-center">
               Lorem ipsum dolor sit amet, vim affert alterum ne. An his adipisci
               dissentias. Vidit debitis in his. Cu cum graeco prodesset, no has
